0e1dc9f5d994b598e354d6a5ac69e7043c413263
eliott Soleil--Moreau <eliott.soleil@gmail.com>
2026-04-24 01:54:30 +0200

    Integration Username

--- src/main/java/org/example/iutprojectdocker/HelloController.java
+++ src/main/java/org/example/iutprojectdocker/HelloController.java
@@ -28,11 +28,16 @@ public class HelloController {
     }
 
 
+    /**
+     * @return A greeting message that includes the value of the "APP_USER" environment variable, or "World" if it is not set.
+     */
     @GetMapping("/")
     public String hello() {
         try {
+            String appUser = System.getenv("APP_USER");
+            String name = (appUser == null || appUser.isEmpty()) ? "World" : appUser;
+            String base = String.format("Hello %s!", name);
             Long count = getHitCountSafe();
-            String base = "Hello World";
             return (count == null)
                     ? base + " (Redis indisponible)\n"
                     : base + String.format(" J'ai été visité %d fois.\n", count);
